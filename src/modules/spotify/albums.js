bd8b7745c014e844f092a51291af0b8a385e0e9d
Matheus Alexandre <mathe.system@gmail.com>
2019-05-18 15:26:00 -0300

    Now the album exports a function with all known functions

--- src/modules/spotify/albums.js
+++ src/modules/spotify/albums.js
@@ -1,13 +1,19 @@
-import { API_URL } from '../../config/consts';
-
-export const getAlbum = (id) =>
-  fetch(`${API_URL}/albums/${id}`)
-    .then(data => ({ album: 'name' }));
-
-export const getAlbums = (ids) =>
-  fetch(`${API_URL}/albums/?ids=${ids}`)
-    .then(data => ({ albums: ['albumA', 'albumB'] }));
-
-export const getAlbumTracks = (id) =>
-  fetch(`${API_URL}/albums/${id}/tracks`)
-    .then(data => ({ track: ['tracks', 'tracks2'] }));
+// import { API_URL } from '../../config/consts';
+export default function album() {
+  return {
+    getAlbum: id => this.request(`${this.apiURL}/albums/${id}`),
+    getAlbums: ids => this.request(`${this.apiURL}/albums/?ids=${ids}`),
+    getTracks: id => this.request(`${this.apiURL}/albums/${id}/tracks`)
+  }
+}
+// export const getAlbum = (id) =>
+//   fetch(`${API_URL}/albums/${id}`)
+//     .then(data => ({ album: 'name' }));
+//
+// export const getAlbums = (ids) =>
+//   fetch(`${API_URL}/albums/?ids=${ids}`)
+//     .then(data => ({ albums: ['albumA', 'albumB'] }));
+//
+// export const getAlbumTracks = (id) =>
+//   fetch(`${API_URL}/albums/${id}/tracks`)
+//     .then(data => ({ track: ['tracks', 'tracks2'] }));
--- src/modules/spotify/index.js
+++ src/modules/spotify/index.js
@@ -7,12 +7,22 @@ import { getAlbums, getAlbumTracks, getAlbum } from './albums';
 //   getAlbums, getAlbumTracks, getAlbum,
 // };
 
+import album from './albums';
+
 import { API_URL } from '../../config/consts';
 
 export default class SpotifyWrapper {
   constructor(options) {
     this.apiURL = options.apiURL || API_URL;
     this.token = options.token;
+
+    // Utilizando as funções que agora estão no obj album
+    // para isso utilizamos o .bind(this) para fazer essa
+    // essa ligação entre o album local com o album que foi
+    // importado na classe, então a partir de agora, é
+    // possível utilizar as funções que estão dentro do
+    // obj album
+    this.album = album.bind(this)();
   }
 
   request(url) {
